c1d57f83fdf208b554f6d6e7d598aeec87fe4f5d
Lesia Hlukhaniuk <olesia.hlukhaniuk@computools.com>
2024-10-22 19:23:04 +0300

    added models and components folders, and created investment-input interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from "./user-input/user-input.component";
+import { HeaderComponent } from './components/header/header.component';
+import { UserInputComponent } from "./components/user-input/user-input.component";
 
 @Component({
   selector: 'app-root',
